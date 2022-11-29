322b8f1e9e7a6eda446d325d519642f4f4cd492a
yochimonji <yochimonji@gmail.com>
2022-11-29 11:42:24 +0900

    add: 途中経過追加

--- run_gradcam.py
+++ run_gradcam.py
@@ -17,14 +17,13 @@ from utils.parse import argparse_gradcam
 def main():
     # 使用するファイルのパスを読み込む
     args = argparse_gradcam()
-    params_path = str(args.params_path) if args.params_path else os.path.join(str(args.dir), "params.example.json")
-    params = utils.load_params(params_path)
-    image_dir_path = str(args.image_dir_path) if args.image_dir_path else str(params["data_path"]["test"])
-    if image_dir_path[-1] != "/":
-        image_dir_path += "/"
+    params = utils.load_params(args, phase="test")
+    image_dir_A_path = str(args.image_dir_A)
+    image_dir_B_path = str(args.image_dir_B)
     weight_path = str(args.weight_path) if args.weight_path else os.path.join(str(args.dir), "weight/weight0.pth")
-    save_dir = os.path.join(str(args.dir), "gradcam" + image_dir_path.replace("./data", "").replace("data", ""))
+    save_dir = os.path.join(str(args.params_path).replace("params.json", ""), "gradcam", args.save_name)
     os.makedirs(save_dir, exist_ok=True)
+    print(save_dir)
 
     # GPUが使用可能ならGPU、不可能ならCPUを使う
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -45,47 +44,43 @@ def main():
     gradcam = GradCAM(net, target_layer)
 
     # 可視化する画像のパスを貯める辞書
-    labels: list = params["labels"]
-    image_paths = {}
-    for l1 in labels:
-        for l2 in labels:
-            image_paths[l1 + "_" + l2] = []
-    image_paths_all = glob(image_dir_path + "/**/*.tif", recursive=True)
+    image_path_list_A = glob(image_dir_A_path + "/**/*.tif", recursive=True)
+    print(len(image_path_list_A))
 
     # ラベルごとに可視化する画像を決定
-    for path in image_paths_all:
-        pil_img = PIL.Image.open(path)
-        torch_img = trans_tensor(pil_img).to(device)
-        normed_torch_img = trans_norm(torch_img)[None]
-        logit = net(normed_torch_img)
+    # for path in image_paths_all:
+    #     pil_img = PIL.Image.open(path)
+    #     torch_img = trans_tensor(pil_img).to(device)
+    #     normed_torch_img = trans_norm(torch_img)[None]
+    #     logit = net(normed_torch_img)
 
-        temp_path = path.replace(image_dir_path, "")
-        actual = temp_path[: temp_path.find("/")]
-        pred = labels[logit.cpu()[0].argmax()]
-        image_paths_key = actual + "_" + pred
+    #     temp_path = path.replace(image_dir_path, "")
+    #     actual = temp_path[: temp_path.find("/")]
+    #     pred = labels[logit.cpu()[0].argmax()]
+    #     image_paths_key = actual + "_" + pred
 
-        if len(image_paths[image_paths_key]) <= 5:
-            image_paths[image_paths_key].append(path)
+    #     if len(image_paths[image_paths_key]) <= 5:
+    #         image_paths[image_paths_key].append(path)
 
-    # 可視化
-    for key, path_list in image_paths.items():
-        if path_list == []:
-            continue
-        images = []
-        for path in path_list:
-            pil_img = PIL.Image.open(path)
-            torch_img = trans_tensor(pil_img).to(device)
-            normed_torch_img = trans_norm(torch_img)[None]
-            mask, _ = gradcam(normed_torch_img)
-            heatmap, result = visualize_cam(mask, torch_img)
-            images.extend([torch_img.cpu(), heatmap, result])
-        grid_image = make_grid(images, nrow=3)
+    # # 可視化
+    # for key, path_list in image_paths.items():
+    #     if path_list == []:
+    #         continue
+    #     images = []
+    #     for path in path_list:
+    #         pil_img = PIL.Image.open(path)
+    #         torch_img = trans_tensor(pil_img).to(device)
+    #         normed_torch_img = trans_norm(torch_img)[None]
+    #         mask, _ = gradcam(normed_torch_img)
+    #         heatmap, result = visualize_cam(mask, torch_img)
+    #         images.extend([torch_img.cpu(), heatmap, result])
+    #     grid_image = make_grid(images, nrow=3)
 
-        filename = "actual_" + key.split("_")[0] + "_pred_" + key.split("_")[1] + ".png"
-        transforms.ToPILImage()(grid_image).save(os.path.join(save_dir, filename))
+    #     filename = "actual_" + key.split("_")[0] + "_pred_" + key.split("_")[1] + ".png"
+    #     transforms.ToPILImage()(grid_image).save(os.path.join(save_dir, filename))
 
-    with open(os.path.join(save_dir, "visualized_image_paths.json"), "w") as file:
-        json.dump(image_paths, file, indent=4)
+    # with open(os.path.join(save_dir, "visualized_image_paths.json"), "w") as file:
+    #     json.dump(image_paths, file, indent=4)
 
 
 if __name__ == "__main__":
--- utils/parse.py
+++ utils/parse.py
@@ -12,17 +12,23 @@ def argparse_train():
     return parser.parse_args()
 
 
-def argparse_test():
+def argparse_base_test():
     parser = argparse_base()
     parser.add_argument("-d", "--dataroot", type=str)
     parser.add_argument("-n", "--name", type=str, required=True)
+    return parser
+
+
+def argparse_test():
+    parser = argparse_base_test()
     return parser.parse_args()
 
 
 def argparse_gradcam():
-    parser = argparse_base()
-    parser.add_argument("-d", "--dir", type=str)
-    parser.add_argument("-i", "--image_dir_path", type=str)
+    parser = argparse_base_test()
+    parser.add_argument("--image_dir_A", type=str, required=True)
+    parser.add_argument("--image_dir_B", type=str, required=True)
+    parser.add_argument("-s", "--save_name", type=str, required=True, help="<dir>/gradcam/<save_name>/に保存する")
     parser.add_argument("-w", "--weight_path", type=str)
-    parser.add_argument("-n", "--output_num", type=int, default=5)
+    parser.add_argument("-n", "--output_num", type=int, default=10)
     return parser.parse_args()
